df17886d9b9e4a02ff9d790ec9461e8ca75ccdc3
RyotaHirano <hirano@tsumikiinc.com>
2016-02-01 18:15:40 +0900

    add line chart

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,13 @@ import Router from './lib/router';
 import commonAction from './common';
 import barChart from './contents/barChart';
 import columnChart from './contents/columnChart';
+import lineChart from './contents/lineChart';
 
 const router = new Router({
   '*': commonAction,
   '/barchart': barChart,
-  '/columnchart': columnChart
+  '/columnchart': columnChart,
+  '/linechart': lineChart
 }, {
   rootPath: ''
 });
